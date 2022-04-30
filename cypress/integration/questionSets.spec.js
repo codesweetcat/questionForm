b04ac9ff53ed2997faadb86d385f29f12f6812cf
haoming <haoming666@hotmail.com>
2022-04-30 17:12:10 +0100

    feat: add cypress into express

---
+++ cypress/integration/questionSets.spec.js
@@ -0,0 +1,40 @@
+
+
+describe("renders the home page", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:3000");
+    });
+    describe("question set API testing", () => {
+        it("fetch question sets - GET", () => {
+            cy.request('/').as('allQuestionSetsRequest');
+            cy.get('@allQuestionSetsRequest').then(questionSets => {
+               expect(questionSets.status).to.eq(200);
+               assert.isArray(questionSets.body.question_sets, 'questionSets Response is an array')
+           })
+          });
+          it("renders correctly", () => {
+            cy.get("form").should("exist");
+          });
+      });
+      
+      describe("Answers API - POST", () => {
+        it("check ALL box and Submit", () => {
+            cy.get('[type="checkbox"]')
+                .as('checkboxes')
+                .check()
+
+            cy.contains('Submit').click()
+            .then(() => {
+                cy.on('window:alert', (str) => {
+                    expect(str).to.equal(`Answers add successfully!`)
+                  })
+            })  
+        });
+
+    })
+
+  
+
+})
+
+
